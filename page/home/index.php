fbb85b3e66bb1bec0829df0af09e01af086dde2d
Guardiantz <andreyova81@gmail.com>
2025-05-25 21:58:58 +0700

    mengubah landing page

---
+++ page/home/index.php
@@ -0,0 +1,12 @@
+<section class="hero is-medium is-primary is-bold">
+    <div class="hero-body">
+        <div class="container">
+            <h1 class="title animate__animated animate__zoomIn">
+                SPK PENILAIAN GURU HONORER MENGGUNAKAN METODE WEIGHTED PRODUCT (WP)
+            </h1>
+            <h2 class="subtitle animate__animated animate__slideInUp">
+                SMK Negeri 1 Jambi
+            </h2>
+        </div>
+    </div>
+</section>
